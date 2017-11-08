f8e9da3a2f349a190022e3ce4e018b5a034c18c7
JohannLiebert1130 <672016243@qq.com>
2017-11-08 16:46:31 +0800

    Version 0.5-UserLogin

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 
-class LoginForm(FlaskForm):
+class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
